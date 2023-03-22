0cda571447e60e24ca4e45cb102aaa7652bf499a
richard200 <richmn200@gmail.com>
2023-03-22 14:25:40 +0300

    creating the frontend react app

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   #user routes
-  post '/users', to: 'users#register'
+  post '/users/register', to: 'users#register'
 
 end
